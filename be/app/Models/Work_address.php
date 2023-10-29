78b6719c0c0db26c9913aa5e92350e2e44496e16
bach-long <nguyenbach19122002@gmail.com>
2023-10-29 09:03:08 +0700

    feature: init be

---
+++ be/app/Models/Work_address.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Work_address extends Model
+{
+    protected $fillable = [
+        'workable_place_id',
+        'profile_id',
+    ];
+
+    protected $primarykey = 'id';
+
+    public $incrementing = true;
+}
