788e1ff36680f1d640bedac869e534b2e28bf1b0
bach-long <nguyenbach19122002@gmail.com>
2023-11-30 20:02:08 +0700

    change url for verification

--- be/resources/views/emails/passwordChange.blade.php
+++ be/resources/views/emails/passwordChange.blade.php
@@ -4,11 +4,11 @@
     <title>Your password was changed</title>
 </head>
 <body>
-  
+
     <h1>Hi, {{ $user->fullname }}</h1>
     <p>{{ $user->email }}</p>
-     
+
     <p>You have changed your password</p>
-    <a href="{{'http://localhost:8000/active/'.$user->token}}">Click here to verify</a>
+    <a href="{{env('APP_ENV') === 'production' ? 'https://recruit.bachnguyencoder.id.vn/active/'.$user->token : 'http://localhost:8000/active/'.$user->token}}">Click here to verify</a>
 </body>
 </html>
--- be/resources/views/emails/userEmail.blade.php
+++ be/resources/views/emails/userEmail.blade.php
@@ -4,11 +4,11 @@
     <title>Verification for your account</title>
 </head>
 <body>
-  
+
     <h1>Hi, {{ $user->fullname }}</h1>
     <p>{{ $user->email }}</p>
-     
+
     <p>Thank you for using our app</p>
-    <a href="{{'http://localhost:8000/active/'.$user->token}}">Verify</a>
+    <a href="{{env('APP_ENV') === 'production' ? 'https://recruit.bachnguyencoder.id.vn/active/'.$user->token : 'http://localhost:8000/active/'.$user->token}}">Verify</a>
 </body>
 </html>
